f21cc2c48577a3ac64d04438847003a827c7712b
sourav-crossml <sourav@crossml.com>
2021-08-17 18:14:21 +0530

    final commit

--- Hiring/candidate_data/admin.py
+++ Hiring/candidate_data/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from . models import candidate_data
-# Register your models here.
 
+"""
+we need to register model candidate_data here too access it from admin pannel 
+
+"""
 admin.site.register(candidate_data)
--- Hiring/candidate_data/models.py
+++ Hiring/candidate_data/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 
-# Create your models here.
-# this model stores data from the user or job seeker and store it in SQLite DB
+
+
+
 class candidate_data(models.Model):
+    """
+    this model stores data from the user or job seeker and store it in SQLite DB
+    """
+    
     candidate_name = models.CharField(max_length=200)
     resume_portfolio_link = models.URLField(max_length=200)
     primary_skills = models.CharField(max_length=200)
     secondary_skills = models.CharField(max_length=200)
     candidate_experince = models.IntegerField()
 
-    #   this function will help in displaying candidate name 
     def __str__(self):
+        """
+        this function will help in displaying candidate name 
+        """
         return self.candidate_name
